03d9e336fd2f189c7cf742b8d71370df4cba81a8
mcraepv <61661042+mcraepv@users.noreply.github.com>
2020-05-10 15:17:39 -0500

    adds time functionality

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 $(document).ready(function () {
+  var time;
+  function updateTime() {
+    time = moment();
+    var dateTime = moment().format("MMMM Do YYYY, h:mm:ss a");
+    $("#currentDay").text(dateTime);
+    setTimeout(updateTime, 1000);
+  }
+  var today = moment().format("L");
+  console.log(today);
+  updateTime();
+  var timeSlots = [];
+  $(".input-group-text").each(function () {
+    var hourID = $(this).attr("id");
+    var timeSlot = moment();
+    timeSlot.minute(0);
+    timeSlot.second(0);
+    timeSlot.hour(hourID);
+    timeSlot.date(today);
+    var slotHour = timeSlot.hour();
+    var thisHour = time.hour();
+    thisInput = $(this).parent().next();
+    function inputTimeChecker() {
+      if (timeSlot.isBefore(time) && slotHour !== thisHour) {
+        thisInput.attr("class", "bg-secondary form-control text-white");
+      } else if (slotHour === thisHour) {
+        thisInput.attr("class", "bg-primary  form-control text-white");
+      } else if (timeSlot.isAfter(time)) {
+        thisInput.attr("class", "bg-success form-control text-white");
+      }
+      setTimeout(inputTimeChecker, 60000);
+    }
+    inputTimeChecker();
+  });
   var events = [];
   var savedEvents = JSON.parse(localStorage.getItem("savedEvents"));
   if (savedEvents !== null) {
     events = savedEvents;
   }
-
   $(".btn").each(function () {
     for (var i = 0; i < events.length; i++) {
       if ($(this).attr("id") === events[i].location) {
